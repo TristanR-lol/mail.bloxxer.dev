5c4ea72c6daa0781d7c718a27a1251ca905f03bc
TristanR-lol <tristan.raneroyay@proton.me>
2026-05-05 20:10:06 +0100

    Fix logins.. AGAIN...

--- api/get-user-email.js
+++ api/get-user-email.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   try {
     const { data, error } = await supabase
       .from('users')
-      .select('newEmail')
+      .select('newemail')
       .eq('id', userId)
       .single()
 
