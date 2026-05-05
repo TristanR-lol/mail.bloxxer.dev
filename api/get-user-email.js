522d14a5cdee255eb8b386398f3bc218886798f8
TristanR-lol <tristan.raneroyay@proton.me>
2026-05-05 20:02:27 +0100

    Tinkering :P

--- api/get-user-email.js
+++ api/get-user-email.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 
     if (error) return res.status(400).json({ error: error.message })
 
-    res.status(200).json({ newEmail: data.newemail })
+    res.status(200).json({ newemail: data.newemail })
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
